536d88c5d4b1cf596a299e358ed1f92da8e2e72f
Vidhilika Gupta <gvidhilika0606@gmail.com>
2025-08-21 12:28:02 +0530

    added environments

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+// export const environment = {
+//   production: false,
+//   firebase: {
+//     apiKey: "AIzaSyAak2WKa96P4Q-cIA2tsAW1NlegvTC70c4",
+//     authDomain: "shopseeapp.firebaseapp.com",
+//     databaseURL: "https://shopseeapp-default-rtdb.firebaseio.com",
+//     projectId: "shopseeapp",
+//     storageBucket: "shopseeapp.firebasestorage.app",
+//     messagingSenderId: "403635044295",
+//     appId: "1:403635044295:web:8d302229db70cbf8a423e2",
+//     measurementId: "G-D7J6FMQS6L"
+//   }
+// };
